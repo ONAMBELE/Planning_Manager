4b122c8edbf5a25dd62c1556ec310f7b87dce70a
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2025-02-14 11:37:23 +0100

    ajout des routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import BoardSpace from './pages/BoardSpace'
 import Workspaces from './pages/Workspaces'
-
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 
 function App() {
 
   return (
     <div>
-      <Workspaces/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Workspaces/>}/>
+          <Route path='/board' element={<BoardSpace/>}/>
+          <Route path='*' element={<Workspaces/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
