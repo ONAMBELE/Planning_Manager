cd78f78dbe7b60e7fa59498cfcffd4f4f59e31f2
ONAMBELE KONI <bryan.koni@facsciences-uy1.cm>
2025-02-14 09:39:40 +0100

    ajout des fontionnalités d'ajout et de suppression des taches

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div>
-      <BoardSpace/>
+      <Workspaces/>
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  //base: "/Planning_Manager/", // ✅ Chemin correct pour GitHub Pages
+  base: "./", // ✅ Chemin correct pour GitHub Pages
 });
